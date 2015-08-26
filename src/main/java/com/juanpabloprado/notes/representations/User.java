f080013fc73624632166ecf3d8503031a7ed29a6
Juan Pablo Prado <juan@juanpabloprado.com>
2015-08-26 16:52:33 -0500

    add validation for email

--- src/main/java/com/juanpabloprado/notes/representations/User.java
+++ src/main/java/com/juanpabloprado/notes/representations/User.java
@@ -1,10 +1,13 @@
 package com.juanpabloprado.notes.representations;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.google.common.base.Objects;
 import com.hubspot.rosetta.annotations.RosettaNaming;
+import io.dropwizard.validation.ValidationMethod;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.internal.constraintvalidators.EmailValidator;
 
 /**
  * Created by Juan on 8/21/2015.
@@ -22,8 +25,6 @@ public class User {
     @Length(min = 2, max = 50)
     private final String password;
 
-//    @NotBlank
-//    @Length(min = 2, max = 89)
     private final String email;
 
     public User(String username, String password) {
@@ -42,6 +43,17 @@ public class User {
         username = null;
         password = null;
         email = null;
+    }
+
+    @JsonIgnore
+    @ValidationMethod(message = "email is not valid")
+    public boolean isValidEmail() {
+        if (email != null ) {
+            EmailValidator emailValidator = new EmailValidator();
+            return emailValidator.isValid(email, null);
+        } else {
+            return true;
+        }
     }
 
     public String getUsername() {
